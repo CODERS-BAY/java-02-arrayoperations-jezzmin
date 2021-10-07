9756e5f4d88e9280367cd9ccb795daf2ba4997d1
jezzmin <jasmin.gelbmann@gmail.com>
2021-10-07 09:29:40 +0200

    Finished

--- lib/src/main/java/exercise/arraysTDD/ArrayOperations.java
+++ lib/src/main/java/exercise/arraysTDD/ArrayOperations.java
@@ -1,5 +1,7 @@
 package exercise.arraysTDD;
 
+import java.util.Arrays;
+
 /**
  * Utility class which performs various operations with integer arrays.
  */
@@ -22,7 +24,9 @@ public class ArrayOperations {
      */
 
     public void print() {
-        
+        for (int number : numbers) {
+            System.out.println(number);
+        }
     }
 
     /**
@@ -30,21 +34,50 @@ public class ArrayOperations {
      * @see <a href="sorting algortihms">http://faculty.cs.niu.edu/~hutchins/csci241/sorting.htm</a>
      */
     public int[] sort() {
-       return null;
+        int sort = 0;
+
+        for (int i = 1; i < numbers.length; i++) {
+            for (int y = 0; y < numbers.length-1; y++) {
+                if (numbers[y] > numbers[y+1]) {
+                    sort = numbers[y];
+                    numbers[y] = numbers[y+1];
+                    numbers[y+1] = sort;
+                }
+            }
+        }
+       return numbers;
     }
 
     /**
      * @return the sorted array in reverse order
      */
     public int[] revertSort() {
-        return null;
+        int sort = 0;
+
+        for (int i = 1; i < numbers.length; i++) {
+            for (int y = 0; y < numbers.length-1; y++) {
+                if (numbers[y] < numbers[y +1]) {
+                    sort = numbers[y];
+                    numbers[y] = numbers[y+1];
+                    numbers[y+1] = sort;
+                }
+            }
+        }
+        return numbers;
     }
 
     /**
      * @return the unsorted array in reverted order.
      */
     public int[] reverted() {
-        return null;
+        int [] array = new int[unsorted.length];
+        int counter = 0;
+
+        for (int i = unsorted.length -1; i >= 0; i--) {
+            array[counter] = numbers[i];
+            counter++;
+        }
+        return array;
     }
 
     /**
@@ -52,6 +85,11 @@ public class ArrayOperations {
      * @return true if the array contains the value, false otherwise.
      */
     public boolean contains(int value) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == value) {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -59,28 +97,54 @@ public class ArrayOperations {
      * @return the average value of all elements summed up.
      */
     public double average() {
-        return 0.0;
+        double sum = 0;
+        for (int i = 0; i < numbers.length;i++) {
+            sum = sum + numbers[i];
+        }
+        return sum / numbers.length;
     }
 
     /**
      * @return the average value of all elements summed up, but without the highest and the lowest value.
      */
     public double trimmedMean() {
-        return 0.0;
+        int highestNumber = 0;
+        int lowestNumber = 0;
+        double sum = 0;
+        for (int i = 0; i < numbers.length;i++) {
+            highestNumber = maxValue();
+            lowestNumber = minValue();
+            sum = sum + numbers[i];
+        }
+        return (sum - (lowestNumber + highestNumber)) / (numbers.length-2);
     }
 
     /**
      * @return the max value of the array. In the array [1,9,3] max would be 9.
      */
     public int maxValue() {
-        return 0;
+        int highestNumber = 0;
+        for(int i = 0; i < numbers.length; i++) {
+            if (highestNumber < numbers[i]) {
+                highestNumber = numbers[i];
+            }
+        }
+        return highestNumber;
     }
 
     /**
      * @return the min value of the array. In the array [1,9,3] min would be 1.
      */
     public int minValue() {
-        return 0;
+        int lowestNumber = 0;
+        for(int i = 0; i < numbers.length; i++) {
+            if (i == 0) {
+                lowestNumber = numbers[i];
+            } else if (numbers[i] < lowestNumber) {
+                lowestNumber = numbers[i];
+            }
+        }
+        return lowestNumber;
     }
 
     /* **********************************************************
